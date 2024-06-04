30e6d1c20e7df0a7a6d41cc751a4086f8da03c41
Jhoseph <jhoseph@localhost.localdomain>
2024-06-03 23:11:00 -0400

    photo

--- jhoseph_caceres/jhoseph_caceres.py
+++ jhoseph_caceres/jhoseph_caceres.py
@@ -16,20 +16,22 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("JHOSEPH CACERES ARAMAYO", size="9"),
+            rx.heading("JHOSEPH CACERES ARAMAYO", class_name="font-bol text-blue-800 text-2xl"),
+            
             rx.image(src="jose.jpg", class_name="w-[400px]"),
             rx.text(
                 "My passion for technology drives me to constantly explore new solutions and stay up to date with the latest advances. I excel at problem solving and find satisfaction in finding innovative solutions. My focus on continuous learning allows me to adapt quickly to changes and constantly improve my skills. I am committed to contributing to the field of computing with creativity and dedication.",
                 
-                class_name="text-2xl text-blue-600"
+                class_name="text-lg text-blue-600"
             ),
+            
             rx.link(
                 rx.button("Facebook"),
                 href="https://www.facebook.com/jhoseph.caceresaramayo/",
                 is_external=True,
             ),
             rx.link(
-                rx.button("Instagram"),
+                                rx.button("Instagram"),
                 href="https://www.instagram.com/jhoseph_caceres19/",
                 is_external=True,
                 class_name="bg-blue-600"
